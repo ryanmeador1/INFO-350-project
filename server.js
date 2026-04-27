117de0d042ad4c713c0fa80e79e33bfa1da6af74
ryanmeador1 <ryanmeador1@gmail.com>
2026-04-27 11:01:52 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require(`express`);
+const app = express();
+
+app.get('/api/message', (req, res) => {
+  res.json({ message: `This is your first API message`});
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
+
+app.use(express.json()); 
+
+app.post('/api/notes',(req,res)=>{
+const{name,note} = req.body; 
+if(!name || !note){
+  return res.status(400).json({error:'Both name and note are required'}); 
+}
+res.status(201).json({message: 'Note recevived!',data:{name,note}}); 
+});
